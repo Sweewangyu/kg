b49cb25b04c5df8bceffb3308b67db6969cfc339
swee <2385455860@qq.com>
2026-03-23 16:46:34 +0800

    遗留

--- experiments/run_re.py
+++ experiments/run_re.py
@@ -2,9 +2,12 @@ import sys
 sys.path.append("./src")
 from models import *
 from dataset_def import *
+
 data_dir = "./data/datasets/NYT11/"
-model = LLaMA("meta-llama/Meta-Llama-3-8B-Instruct")
+model_name = sys.argv[1] if len(sys.argv) > 1 else "gpt-4o-mini"
+api_key = sys.argv[2] if len(sys.argv) > 2 else ""
+base_url = sys.argv[3] if len(sys.argv) > 3 else "https://api.openai.com/v1"
+model = OpenAIModel(model_name_or_path=model_name, api_key=api_key, base_url=base_url)
 dataset = REDataset(name="NYT11", data_dir=data_dir)
 f1_score = dataset.evaluate(llm=model, mode="quick")
 print("f1_score: ", f1_score)
-
